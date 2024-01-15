c65a9f4d05643aa03c0ce885cdb9d86f4987e638
Hju Kneyck Flores <hjucode@gmail.com>
2024-01-15 17:11:42 +0800

    Use react-router-dom's `Link` component for better UX

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,76 @@
-import React from "react";
-import logo from "../resources/temp-pawpal-logo.png";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../resources/temp-pawpal-logo.png';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <div
-      className="bg-gray-900 py-3 px-6 shadow-2 flex align-items-center justify-content-between relative border-bottom-1 border-gray-800"
-    >
-      <img
-        src={logo}
-        height="40"
-        className="mr-0 lg:mr-6"
-      />
+    <div className="bg-gray-900 py-3 px-6 shadow-2 flex align-items-center justify-content-between relative border-bottom-1 border-gray-800">
+      <img alt="PawPal Logo" src={logo} height="40" className="mr-0 lg:mr-6" />
       <div className="align-items-center flex-grow-1 justify-content-between hidden lg:flex absolute lg:static w-full bg-gray-900 left-0 top-100 z-1 shadow-2 lg:shadow-none border-1 lg:border-none border-gray-800">
         <ul className="list-none p-0 m-0 flex lg:align-items-center select-none flex-column lg:flex-row">
           <li>
-            <a href="/pawpal/home" style={{textDecoration:"none"}} className="p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150 w-full">
+            <Link
+              to="/pawpal/home"
+              className="no-underline p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150 w-full"
+            >
               <i className="pi pi-home mr-2"></i>
               <span>Home</span>
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="/pawpal/adopt" style={{textDecoration:"none"}} className="p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150 w-full">
-              <svg className="mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M226.5 92.9c14.3 42.9-.3 86.2-32.6 96.8s-70.1-15.6-84.4-58.5s.3-86.2 32.6-96.8s70.1 15.6 84.4 58.5zM100.4 198.6c18.9 32.4 14.3 70.1-10.2 84.1s-59.7-.9-78.5-33.3S-2.7 179.3 21.8 165.3s59.7 .9 78.5 33.3zM69.2 401.2C121.6 259.9 214.7 224 256 224s134.4 35.9 186.8 177.2c3.6 9.7 5.2 20.1 5.2 30.5v1.6c0 25.8-20.9 46.7-46.7 46.7c-11.5 0-22.9-1.4-34-4.2l-88-22c-15.3-3.8-31.3-3.8-46.6 0l-88 22c-11.1 2.8-22.5 4.2-34 4.2C84.9 480 64 459.1 64 433.3v-1.6c0-10.4 1.6-20.8 5.2-30.5zM421.8 282.7c-24.5-14-29.1-51.7-10.2-84.1s54-47.3 78.5-33.3s29.1 51.7 10.2 84.1s-54 47.3-78.5 33.3zM310.1 189.7c-32.3-10.6-46.9-53.9-32.6-96.8s52.1-69.1 84.4-58.5s46.9 53.9 32.6 96.8s-52.1 69.1-84.4 58.5z"/></svg>
+            <Link
+              to="/pawpal/adopt"
+              className="no-underline p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150 w-full"
+            >
+              <svg
+                className="mr-2"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                height="16"
+                width="16"
+                viewBox="0 0 512 512"
+              >
+                <path d="M226.5 92.9c14.3 42.9-.3 86.2-32.6 96.8s-70.1-15.6-84.4-58.5s.3-86.2 32.6-96.8s70.1 15.6 84.4 58.5zM100.4 198.6c18.9 32.4 14.3 70.1-10.2 84.1s-59.7-.9-78.5-33.3S-2.7 179.3 21.8 165.3s59.7 .9 78.5 33.3zM69.2 401.2C121.6 259.9 214.7 224 256 224s134.4 35.9 186.8 177.2c3.6 9.7 5.2 20.1 5.2 30.5v1.6c0 25.8-20.9 46.7-46.7 46.7c-11.5 0-22.9-1.4-34-4.2l-88-22c-15.3-3.8-31.3-3.8-46.6 0l-88 22c-11.1 2.8-22.5 4.2-34 4.2C84.9 480 64 459.1 64 433.3v-1.6c0-10.4 1.6-20.8 5.2-30.5zM421.8 282.7c-24.5-14-29.1-51.7-10.2-84.1s54-47.3 78.5-33.3s29.1 51.7 10.2 84.1s-54 47.3-78.5 33.3zM310.1 189.7c-32.3-10.6-46.9-53.9-32.6-96.8s52.1-69.1 84.4-58.5s46.9 53.9 32.6 96.8s-52.1 69.1-84.4 58.5z" />
+              </svg>
               <span>Adopt</span>
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="/pawpal/about" style={{textDecoration:"none"}} className="p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150 w-full">
+            <Link
+              to="/pawpal/about"
+              className="no-underline p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150 w-full"
+            >
               <i className="pi pi-info-circle mr-2"></i>
               <span>About Us</span>
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="/pawpal/donate" style={{textDecoration:"none"}} className="p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150 w-full">
-              <svg className="mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="16" width="18" viewBox="0 0 576 512"><path d="M512 80c8.8 0 16 7.2 16 16v32H48V96c0-8.8 7.2-16 16-16H512zm16 144V416c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V224H528zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm56 304c-13.3 0-24 10.7-24 24s10.7 24 24 24h48c13.3 0 24-10.7 24-24s-10.7-24-24-24H120zm128 0c-13.3 0-24 10.7-24 24s10.7 24 24 24H360c13.3 0 24-10.7 24-24s-10.7-24-24-24H248z"/></svg>
+            <Link
+              to="/pawpal/donate"
+              className="no-underline p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150 w-full"
+            >
+              <svg
+                className="mr-2"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                height="16"
+                width="18"
+                viewBox="0 0 576 512"
+              >
+                <path d="M512 80c8.8 0 16 7.2 16 16v32H48V96c0-8.8 7.2-16 16-16H512zm16 144V416c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V224H528zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm56 304c-13.3 0-24 10.7-24 24s10.7 24 24 24h48c13.3 0 24-10.7 24-24s-10.7-24-24-24H120zm128 0c-13.3 0-24 10.7-24 24s10.7 24 24 24H360c13.3 0 24-10.7 24-24s-10.7-24-24-24H248z" />
+              </svg>
               <span>Donate</span>
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="/pawpal/missing" style={{textDecoration:"none"}} className="p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150 w-full">
+            <Link
+              to="/pawpal/missing"
+              className="no-underline p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150 w-full"
+            >
               <i className="pi pi-search mr-2"></i>
               <span>Missing</span>
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,28 +1,50 @@
-import { Button } from "primereact/button";
-import bg from "../../resources/home_bg.jpg"
-
+import { Button } from 'primereact/button';
+import bg from '../../resources/home_bg.jpg';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
-    return (
+  return (
     <div
-        className="flex align-items-center min-h-screen bg-center bg-no-repeat bg-cover"
-        style={{ backgroundImage: `url("${bg}")` }}
+      className="flex align-items-center min-h-screen bg-center bg-no-repeat bg-cover"
+      style={{ backgroundImage: `url("${bg}")` }}
     >
-        <div className="flex">
-            <div className="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center ">
-                <section>
-                    <span className="block text-8xl font-bold mb-1 text-blue-900" style={{textShadow: "-2px -1px 0 #ffffff"}}>Adopt Love. Change a Life.</span>
-                    <span className="block text-2xl line-height-3 text-blue-900 mb-4" style={{textShadow: "-1px 0 #ffffff"}}>
-                        Discover the joy of giving a rescue pet a loving home.
-                        Join our mission to make second chances happen for countless deserving animals.
-                        Together, we can turn compassion into action.
-                    </span>
+      <div className="flex">
+        <div className="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center ">
+          <section>
+            <span
+              className="block text-8xl font-bold mb-1 text-blue-900"
+              style={{ textShadow: '-2px -1px 0 #ffffff' }}
+            >
+              Adopt Love. Change a Life.
+            </span>
+            <span
+              className="block text-2xl line-height-3 text-blue-900 mb-4"
+              style={{ textShadow: '-1px 0 #ffffff' }}
+            >
+              Discover the joy of giving a rescue pet a loving home. Join our
+              mission to make second chances happen for countless deserving
+              animals. Together, we can turn compassion into action.
+            </span>
 
-                    <a href="/pawpal/adopt"><Button label="Adopt Now" type="button" className="mr-3 p-button-raised" severity="info"></Button></a>
-                    <a href="/pawpal/donate"><Button label="Donate" type="button" severity="primary" raised></Button></a>
-                </section>
-            </div>
+            <Link to="/pawpal/adopt">
+              <Button
+                label="Adopt Now"
+                type="button"
+                className="mr-3 p-button-raised"
+                severity="info"
+              ></Button>
+            </Link>
+            <Link to="/pawpal/donate">
+              <Button
+                label="Donate"
+                type="button"
+                severity="primary"
+                raised
+              ></Button>
+            </Link>
+          </section>
         </div>
+      </div>
     </div>
-    )
+  );
 }
